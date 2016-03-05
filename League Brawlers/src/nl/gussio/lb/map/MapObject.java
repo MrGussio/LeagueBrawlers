5ad301533a3db3f738cdee3e3858fa0c2db9d629
MrGussio <jellehulter@hotmail.com>
2016-03-05 12:37:34 +0100

    Added getters and added a collision rectangle.

--- League Brawlers/src/nl/gussio/lb/map/MapObject.java
+++ League Brawlers/src/nl/gussio/lb/map/MapObject.java
@@ -1,9 +1,11 @@
 package nl.gussio.lb.map;
 
 import java.awt.Graphics;
+import java.awt.Rectangle;
 
 public abstract class MapObject {
 
+	Rectangle collision;
 	protected int x, y, width, height;
 	
 	public MapObject(int x, int y, int width, int height){
@@ -11,9 +13,30 @@ public abstract class MapObject {
 		this.y = y;
 		this.width = width;
 		this.height = height;
+		collision = new Rectangle(x,y,width,height);
 	}
 	
 	public abstract void update();
 	public abstract void render(Graphics g);
+	
+	public Rectangle collision(){
+		return collision;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
 	
 }
