10f05b3437c5e13ca04a49bc2c24f0b4df4e693c
MrGussio <jellehulter@hotmail.com>
2016-03-06 16:58:04 +0100

    The Window class now contains the keylistener

--- League Brawlers/src/nl/gussio/lb/Window.java
+++ League Brawlers/src/nl/gussio/lb/Window.java
@@ -2,13 +2,17 @@ package nl.gussio.lb;
 
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 
-public class Window extends JFrame {
+public class Window extends JFrame implements KeyListener{
 
 	private static final long serialVersionUID = 1L;
 
+	Screen screen;
+	
 	public Window(int width, int height, String title){
 		setSize(new Dimension(width, height));
 		setTitle(title);
@@ -17,12 +21,43 @@ public class Window extends JFrame {
 		setResizable(false);
 		setLocationRelativeTo(null);
 		init();
+		addKeyListener(this);
 	}
 	
 	public void init(){
 		setLayout(new GridLayout(1, 1, 0, 0));
-		Screen screen = new Screen();
+		screen = new Screen();
 		add(screen);
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_A){
+			screen.p.left = true;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_D){
+			screen.p.right = true;			
+		}
+		if(e.getKeyCode() == KeyEvent.VK_W){
+			screen.p.jumping = true;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_A){
+			screen.p.left = false;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_D){
+			screen.p.right = false;			
+		}
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
