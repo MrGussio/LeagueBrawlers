74138e89c2fbf4a6dbe7c06602d3ceeb119f6b7a
MrGussio <jellehulter@hotmail.com>
2016-03-06 16:59:11 +0100

    Added a method to check if an entity collides with another entity or mapobject.

--- League Brawlers/src/nl/gussio/lb/entities/Entity.java
+++ League Brawlers/src/nl/gussio/lb/entities/Entity.java
@@ -1,6 +1,9 @@
 package nl.gussio.lb.entities;
 
 import java.awt.Graphics;
+import java.awt.Point;
+
+import nl.gussio.lb.map.MapObject;
 
 public abstract class Entity {
 
@@ -15,5 +18,9 @@ public abstract class Entity {
 	
 	public abstract void render(Graphics g);
 	public abstract void update();
+	
+	public boolean collides(Point p, MapObject o){
+		return o.collision().contains(p);
+	}
 	
 }
